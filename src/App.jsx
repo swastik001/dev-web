72dfc0d43adc4f4e6496fc705a8a310ce6a74a86
Swastik <swastik.ojha9@gmail.com>
2026-08-25 00:41:02 +0530

    added tailwind, daisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,48 @@
+import "./App.css";
+
 function App() {
-  return <h1>yoo</h1>;
+  return (
+    <>
+      <div class="navbar bg-base-300 shadow-sm">
+        <div class="flex-1">
+          <a class="btn btn-ghost text-xl">DevTinder</a>
+        </div>
+        <div class="flex gap-2">
+          <div class="dropdown dropdown-end mx-5">
+            <div
+              tabindex="0"
+              role="button"
+              class="btn btn-ghost btn-circle avatar"
+            >
+              <div class="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
+            </div>
+            <ul
+              tabindex="-1"
+              class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a class="justify-between">
+                  Profile
+                  <span class="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
